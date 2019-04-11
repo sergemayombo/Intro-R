75e3b4f16943009b049daa8fb92ee254e5134e8f
sergemayombo <sergemayombo@yahoo.fr>
2019-04-11 10:56:54 +0200

    Scale bar fix

---
+++ Mapping with style.R
@@ -0,0 +1,42 @@
+# Mapping with style
+# 11 April 2019
+# Serge
+
+# Load libraries
+library(tidyverse)
+library(scales)
+library(ggsn)
+
+# Load Africa map
+load("data/africa_map.RData")
+
+ggplot() +
+  borders() + # The global shape file
+  coord_equal() # Equal sizing for lon/lat
+
+sa_1 <- ggplot() +
+  borders(fill = "grey70", colour = "black") +
+  coord_equal(xlim = c(12, 36), ylim = c(-38, -22), expand = 0) # Force lon/lat extent
+sa_1
+
+sa_2 <- sa_1 +
+  annotate("text", label = "Atlantic\nOcean", # "n" write Atlantic Ocean in 2 separate lines
+           x = 15.1, y = -32.0,
+           size = 5.0,
+           angle = 30,
+           colour = "navy") +
+  annotate("text", label = "Indian\nOcean",
+           x = 33.2, y = -34.2,
+           size = 5.0,
+           angle = 330,
+           colour = "springgreen")
+sa_2
+
+
+sa_3 <- sa_2 +
+  scalebar(x.min = 22, x.max = 26, y.min = -36, y.max = -35, # Set location of bar
+           dist = 200, height = 1, st.dist = 0.8, st.size = 4, # Set particulars
+           dd2km = TRUE, model = "WGS84") + # Set appearance
+  north(x.min = 22.5, x.max = 25.5, y.min = -33, y.max = -31, # Set location of symbol
+        scale = 1.2, symbol = 16)
+sa_3
